01286d6e7e83ea988b25ddf98014769cc7028172
Leanne Hagger <leannehagger@gmail.com>
2019-01-25 12:22:30 +0000

    updated sentence

--- ch15_weatherApp/uv_request.py
+++ ch15_weatherApp/uv_request.py
@@ -25,4 +25,10 @@ date_and_time = data[0]['date_iso']
 
 print("\nAnd the UV index will be {} on {}.  \n\nThe purpose of this index is to help people protect themselves from UV radiation. " .format(uv_indicator, date_and_time.title()))
 
-    
+     
+# PRINTS OUT:
+
+
+#And the UV index will be 0.38 on 2019-01-26T12:00:00Z.  
+#
+#The purpose of this index is to help people protect themselves from UV radiation. 
